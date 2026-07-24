f0f5acf252336567e130ed3bb56370d4cad589c3
cll <cll@qq.com>
2026-07-24 09:57:31 +0800

    Add PM2 deployment config

---
+++ ecosystem.config.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  apps: [
+    {
+      name: 'deskpet',
+      cwd: '/www/wwwroot/deskpet',
+      script: '/www/wwwroot/deskpet/server.js',
+      interpreter: '/www/server/nodejs/v24.18.0/bin/node',
+      exec_mode: 'fork',
+      instances: 1,
+      watch: false,
+      max_memory_restart: '1024M',
+      env: {
+        NODE_ENV: 'production',
+        DESKPET_PUBLIC_URL: 'https://desktoppet.online',
+        DESKPET_DATA_DIR: '/www/deskpet-data',
+        DESKPET_HTTP_HOST: '127.0.0.1',
+        DESKPET_HTTP_PORT: '3100',
+        DESKPET_TRUST_PROXY: 'true',
+        DESKPET_ADMIN_LOOPBACK_ONLY: 'false',
+        DESKPET_SIGNING_PRIVATE_KEY: '/www/deskpet-data/signing-private.pem',
+        DESKPET_BOOTSTRAP_VERSION: '2.1.0'
+      }
+    }
+  ]
+};
